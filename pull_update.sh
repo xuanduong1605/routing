3f5baae508ec46c4401b8db3a8ab4b60b1fa1981
Vic <wfeng@g.harvard.edu>
2023-02-13 00:08:05 -0500

    Update pull_update.sh

--- pull_update.sh
+++ pull_update.sh
@@ -1,4 +1,12 @@
 #! /bin/bash
 
-git remote add upstream "https://github.com/Harvard-CS145/cs145-21-project2.git"
+res=$(git remote -v | grep upstream)
+
+if [ -z "$res" ]
+then
+    # Depends on whether you want to use https or ssh link to clone
+    # git remote add upstream "https://github.com/Harvard-CS145/cs145-23-project1.git"
+    git remote add upstream "git@github.com:Harvard-CS145/cs145-23-project1.git"
+fi
+
 git pull upstream main --allow-unrelated-histories
